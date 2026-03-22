4e4fccdcfe06c2d34d78461f68b7a9acd7c7ec54
Adrian Mendoza Rodríguez <adrianmendoza@thehiveteam.com>
2026-03-22 01:53:03 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-4771511110-9901e",
+  "appId": "1:525645719871:web:aaec04f1edcb192eb2cb8a",
+  "apiKey": "AIzaSyDybdeOl0RrcWkUvNBp-YGbYSKOxqkZjE8",
+  "authDomain": "studio-4771511110-9901e.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "525645719871"
+};
